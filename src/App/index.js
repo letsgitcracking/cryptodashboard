a55df62a33fed9b6da072ed06b2847dd107af2da
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2019-04-06 23:57:12 +0100

    Restructured files

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import WelcomeMessage from './WelcomeMessage';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <WelcomeMessage />
+      </div>
+    );
+  }
+}
+
+export default App;
