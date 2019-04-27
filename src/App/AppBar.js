b7c75c14795f8ee7ef7079af1d0e61d9e91b830f
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2019-04-27 19:21:53 +0100

    Fix Dashboard firstVisit bug

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -15,6 +15,9 @@ const ControlButtonElem = styled.div`
     ${props => props.active && css`
         text-shadow: 0px 0px 60px #03ff03;
     `}
+    ${props => props.hidden && css`
+        display: none;
+    `}
 `
 
 function toProperCase(lower) {
@@ -24,10 +27,11 @@ function toProperCase(lower) {
 function ControlButton({ name }) {
     return (
         <AppContext.Consumer>
-            {({ page, setPage }) => (
+            {({ firstVisit, page, setPage }) => (
                 <ControlButtonElem
                     active={page === name}
                     onClick={() => setPage(name)}
+                    hidden={firstVisit && name === 'dashboard'}
                 >
                     {toProperCase(name)}
                 </ControlButtonElem>
