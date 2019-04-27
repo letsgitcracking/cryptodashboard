82d497e411ec7914040c37b8da7cd0d048f7bdc4
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2019-04-27 19:46:53 +0100

    Add ChartSelect

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -6,7 +6,7 @@ const cc = require("cryptocompare");
 export const AppContext = React.createContext();
 
 const MAX_FAVORITES = 10;
-const TIME_UNITS = 10;
+const TIME_UNITS = 18;
 
 export class AppProvider extends React.Component {
   constructor(props) {
@@ -14,6 +14,7 @@ export class AppProvider extends React.Component {
     this.state = {
       page: "dashboard",
       favorites: ["BTC", "ETH", "XMR", "DOGE"],
+      timeInterval: 'months',
       ...this.savedSettings(),
       setPage: this.setPage,
       addCoin: this.addCoin,
@@ -21,7 +22,8 @@ export class AppProvider extends React.Component {
       isInFavorites: this.isInFavorites,
       confirmFavorites: this.confirmFavorites,
       setCurrentFavorite: this.setCurrentFavorite,
-      setFilteredCoins: this.setFilteredCoins
+      setFilteredCoins: this.setFilteredCoins,
+      changeChartSelect: this.changeChartSelect,
     };
   }
 
@@ -49,7 +51,7 @@ export class AppProvider extends React.Component {
       {
         name: this.state.currentFavorite,
         data: results.map((ticker, index) => [
-          moment().subtract({months: TIME_UNITS - index}).valueOf(),
+          moment().subtract({[this.state.timeInterval]: TIME_UNITS - index}).valueOf(),
           ticker.USD
         ])
       }
@@ -78,7 +80,7 @@ export class AppProvider extends React.Component {
           this.state.currentFavorite,
           ['USD'],
           moment()
-          .subtract({months: units})
+          .subtract({[this.state.timeInterval]: units})
           .toDate()
           )
       )
@@ -151,6 +153,11 @@ export class AppProvider extends React.Component {
   setPage = page => this.setState({ page });
 
   setFilteredCoins = filteredCoins => this.setState({ filteredCoins });
+
+  changeChartSelect = (value) => {
+    console.log(value);
+    this.setState({timeInterval: value, historical: null}, this.fetchHistorical);
+  }
 
   render() {
     return (
